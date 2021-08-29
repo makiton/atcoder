ccb65afd111ace32278c8f402d02be61a541e33d
makiton <makiton@gmail.com>
2021-08-29 23:12:37 +0900

    Solve abc216/d

--- abc216/d.py
+++ abc216/d.py
@@ -1,15 +1,32 @@
 import sys
+from collections import deque
 
 n, m = map(int, sys.stdin.readline().split())
 
-deps = [[] for _ in range(n)]
-for _ in range(m):
+t = []
+q = deque()
+mem = [[] for _ in range(n)]
+for i in range(m):
     k = int(sys.stdin.readline())
-    aij = list(map(lambda s: int(s) - 1, sys.stdin.readline().split()))
-    for j in range(k):
-        for jj in range(j + 1, k):
-            deps[aij[jj]].append(aij[j])
-            if aij[jj] in deps[aij[j]]:
-                print("No")
-                exit()
-print("Yes")
+    qq = deque(map(lambda s: int(s) - 1, sys.stdin.readline().split()))
+    t.append(qq)
+    mem[qq[0]].append(i)
+    if len(mem[qq[0]]) == 2:
+        q.append(qq[0])
+
+c = 0
+while q:
+    nn = q.popleft()
+    c += 1
+    for i in mem[nn]:
+        qq = t[i]
+        qq.popleft()
+        if qq:
+            mem[qq[0]].append(i)
+            if len(mem[qq[0]]) == 2:
+                q.append(qq[0])
+
+if c == n:
+    print("Yes")
+else:
+    print("No")
